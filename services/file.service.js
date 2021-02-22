f123e064927287b9a64f18cd9ec70908d88e8995
Rotem Carmon <tetch6@gmail.com>
2021-02-22 13:35:46 +0200

    fix read file error

--- services/file.service.js
+++ services/file.service.js
@@ -5,7 +5,7 @@ module.exports = {
   readFromFile
 }
 
-function writeTofile(filePath,data) {
+function writeTofile(filePath, data) {
   const dataToWrite = JSON.stringify(data)
   return new Promise((resolve, reject) => {
     fs.writeFile(filePath, dataToWrite, (err) => {
@@ -17,8 +17,12 @@ function writeTofile(filePath,data) {
 
 function readFromFile(filePath) {
   return new Promise((resolve, reject) => {
+    if (!fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, '[]', 'utf8')
+    }
     fs.readFile(filePath, 'utf-8', (err, data) => {
-      if (err) reject(`error reading from ${filePath}:`, err);
+      if (err) return reject(`error reading from ${filePath}:`, err);
+      console.log('DATAAAAAAAAAAAAAAAAAA IS: ' + data);
       if (!data || !data.length) resolve(null)
       var parsedData = JSON.parse(data)
       resolve(parsedData);
